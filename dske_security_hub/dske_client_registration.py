b7825a1a24a4085880efc73550ec5b251ef0bbb8
Bruno Rijsman <brunorijsman@hotmail.com>
2025-03-06 15:16:20 -0600

    Use names instead of UUIDs

--- dske_security_hub/dske_client_registration.py
+++ dske_security_hub/dske_client_registration.py
@@ -2,17 +2,15 @@
 DSKE Client Registration.
 """
 
-from uuid import UUID
-
 class DSKEClientRegistration:
     """
     A Distributed Secure Key Exchange (DSKE) client registration.
     """
-    _dske_secure_hub_uuid: UUID
-    _dske_client_uuid: UUID
+    _dske_secure_hub_name: str
+    _dske_client_name: str
     _pre_shared_key: bytes
 
-    def __init__(self, dske_secure_hub_uuid: UUID, dske_client_uuid: UUID, pre_shared_key: bytes):
-        self._dske_secure_hub_uuid = dske_secure_hub_uuid
-        self._dske_client_uuid = dske_client_uuid
+    def __init__(self, dske_secure_hub_name: str, dske_client_name: str, pre_shared_key: bytes):
+        self._dske_secure_hub_name = dske_secure_hub_name
+        self._dske_client_name = dske_client_name
         self._pre_shared_key = pre_shared_key
