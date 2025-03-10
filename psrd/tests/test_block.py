3cb35a814cdb4503901b36787c000f57526394de
Bruno Rijsman <brunorijsman@hotmail.com>
2025-03-10 09:12:00 -0600

    Add test_create_random_psrd_block

--- psrd/tests/test_block.py
+++ psrd/tests/test_block.py
@@ -16,35 +16,39 @@ def _bytes_test_pattern(size):
 
 
 def test_block_init():
+    size = 1000
     uuid = uuid4()
-    data = _bytes_test_pattern(1000)
+    data = _bytes_test_pattern(size)
     _block = Block(uuid, data)
 
 
 def test_block_properties():
+    size = 1000
     uuid = uuid4()
-    data = _bytes_test_pattern(1000)
+    data = _bytes_test_pattern(size)
     block = Block(uuid, data)
     assert block.uuid == uuid
-    assert block.remaining_size == 1000
+    assert block.remaining_size == size
 
 
 def test_block_to_management_json():
+    size = 20
     uuid = uuid4()
-    data = _bytes_test_pattern(20)
+    data = _bytes_test_pattern(size)
     block = Block(uuid, data)
     management_json = block.to_management_json()
     assert management_json == {
         "uuid": str(uuid),
-        "original_size": 20,
-        "remaining_size": 20,
+        "original_size": size,
+        "remaining_size": size,
         "data": common.bytes_to_str(data, truncate=True),
     }
 
 
 def test_block_to_protocol_json():
+    size = 20
     uuid = uuid4()
-    data = _bytes_test_pattern(20)
+    data = _bytes_test_pattern(size)
     block = Block(uuid, data)
     protocol_json = block.to_protocol_json()
     assert protocol_json == {
@@ -54,12 +58,19 @@ def test_block_to_protocol_json():
 
 
 def test_block_from_protocol_json():
+    size = 20
     uuid = uuid4()
-    data = _bytes_test_pattern(20)
+    data = _bytes_test_pattern(size)
     protocol_json = {
         "uuid": str(uuid),
         "data": common.bytes_to_str(data),
     }
     block = Block.from_protocol_json(protocol_json)
     assert block.uuid == uuid
-    assert block.remaining_size == 20
+    assert block.remaining_size == size
+
+
+def test_create_random_psrd_block():
+    size = 100
+    block = Block.create_random_psrd_block(size)
+    assert block.remaining_size == size
