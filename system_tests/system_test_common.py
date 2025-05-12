fe4f7bb054a2007c22f63daffdc1baf1522990cd
Bruno Rijsman <brunorijsman@hotmail.com>
2025-05-11 23:23:06 -0100

    Improve start top system test

--- system_tests/system_test_common.py
+++ system_tests/system_test_common.py
@@ -2,12 +2,17 @@
 Common functions for the system tests.
 """
 
-from subprocess import Popen
-from time import sleep
+import os
+import subprocess
+import time
 
 
 _DEFAULT_TOPOLOGY_FILE = "topology.yaml"
+_DEFAULT_TOPOLOGY_CLIENTS = ["carol", "celia", "cindy", "connie", "curtis"]
+# TODO: Is it hilary or hillary?
+_DEFAULT_TOPOLOGY_HUBS = ["hank", "helen", "hillary", "holly", "hugo"]
 _NODE_START_DELAY = 1.0
+_INITIAL_NODE_PORT = 8000
 
 
 def start_topology(topology_file=_DEFAULT_TOPOLOGY_FILE):
@@ -15,8 +20,14 @@ def start_topology(topology_file=_DEFAULT_TOPOLOGY_FILE):
     Start a topology.
     """
     args = [topology_file, "start"]
-    _run_manager(args)
-    sleep(_NODE_START_DELAY)
+    output = _run_manager(args)
+    expected_output = ""
+    for hub in _DEFAULT_TOPOLOGY_HUBS:
+        expected_output += f"Starting hub {hub} on port {_hub_port(hub)}\n"
+    for client in _DEFAULT_TOPOLOGY_CLIENTS:
+        expected_output += f"Starting client {client} on port {_client_port(client)}\n"
+    assert output == expected_output
+    time.sleep(_NODE_START_DELAY)
 
 
 def stop_topology(topology_file=_DEFAULT_TOPOLOGY_FILE):
@@ -24,19 +35,41 @@ def stop_topology(topology_file=_DEFAULT_TOPOLOGY_FILE):
     Stop a topology.
     """
     args = [topology_file, "stop"]
-    _run_manager(args)
+    output = _run_manager(args)
+    expected_output = ""
+    for client in _DEFAULT_TOPOLOGY_CLIENTS:
+        expected_output += f"Stopping client {client} on port {_client_port(client)}\n"
+    for hub in _DEFAULT_TOPOLOGY_HUBS:
+        expected_output += f"Stopping hub {hub} on port {_hub_port(hub)}\n"
+    assert output == expected_output
+
+
+def _hub_port(hub):
+    port = _INITIAL_NODE_PORT
+    port += _DEFAULT_TOPOLOGY_HUBS.index(hub)
+    return port
+
+
+def _client_port(client):
+    port = _INITIAL_NODE_PORT
+    port += len(_DEFAULT_TOPOLOGY_HUBS)
+    port += _DEFAULT_TOPOLOGY_CLIENTS.index(client)
+    return port
 
 
 def _run_manager(args):
     """
     Run the manager.
     """
-    # TODO: Append to file system_test.out instead of overwriting on each run
-    out_filename = "system_test.out"
-    with open(out_filename, "w", encoding="utf-8") as out_file:
-        with Popen(
-            ["python", "-m", "manager"] + args,
-            stdout=out_file,
-            stderr=out_file,
-        ) as _process:
-            pass
+    # TODO: Better method for choosing the directory where manager.py lives
+    os.chdir("/Users/brunorijsman/git-personal/dske-python")
+    result = subprocess.run(
+        ["python", "manager.py"] + args,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        check=False,
+    )
+    assert result.returncode == 0
+    assert result.stderr == b""
+    output = result.stdout.decode(encoding="utf-8")
+    return output
