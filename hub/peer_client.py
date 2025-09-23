b3e688537d7b95bd47a2b6886b34704c180e6d38
Bruno Rijsman <brunorijsman@hotmail.com>
2025-09-23 18:56:25 +0200

    Remove debug prints

--- hub/peer_client.py
+++ hub/peer_client.py
@@ -81,30 +81,15 @@ class PeerClient:
         Check the signature on a FastAPI request. Raise an exception if the signature is invalid.
         """
         received_signature = Signature.from_headers(raw_request.headers)
-        print(
-            f"check_request_signature: {received_signature=}", file=sys.stderr
-        )  # TODO $$$
-        print(
-            f"check_request_signature: {received_signature.signing_key_allocation_enc_str=}",
-            file=sys.stderr,
-        )  # TODO $$$
         allocation = Allocation.from_enc_str(
             received_signature.signing_key_allocation_enc_str, self._client_pool
         )
         allocation.mark_allocated()
-        print(f"check_request_signature: {allocation=}", file=sys.stderr)  # TODO $$$
         signing_key = SigningKey(allocation)
-        print(f"check_request_signature: {signing_key=}", file=sys.stderr)  # TODO $$$
         query = raw_request.scope.get("query_string", b"")
         body = await raw_request.body()
-        print(f"{query=}", file=sys.stderr)  # TODO $$$
-        print(f"{body=}", file=sys.stderr)  # TODO $$$
         computed_signature = signing_key.sign([query, body])
-        print(
-            f"check_request_signature: {computed_signature=}", file=sys.stderr
-        )  # TODO $$$
         signature_ok = received_signature.same_as(computed_signature)
-        print(f"check_request_signature: {signature_ok=}", file=sys.stderr)  # TODO $$$
         if not signature_ok:
             # TODO: Better exception, that causes a 403 forbidden response
             raise ValueError("Invalid signature")
