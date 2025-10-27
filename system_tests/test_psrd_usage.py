4efe570e4c46d60cf078d540c6b656d99a8da6eb
Bruno Rijsman <brunorijsman@hotmail.com>
2025-10-27 19:43:36 +0100

    Improve PSRD usage test

--- system_tests/test_psrd_usage.py
+++ system_tests/test_psrd_usage.py
@@ -2,18 +2,13 @@
 System test for usage of PSRD.
 """
 
-import sys
 import pytest
 from . import system_test_common
 
 
 _CLIENTS = ["carol", "celia", "cindy", "connie", "curtis"]
 _HUBS = ["hank", "helen", "hilary", "holly", "hugo"]
-_AUTHENTICATION_KEY_SIZE_IN_BYTES = 4
-
-_START_REQUEST_PSRD_THRESHOLD = 500
-_STOP_REQUEST_PSRD_THRESHOLD = 2000
-_GET_PSRD_BLOCK_SIZE = 2000
+_SIGNING_KEY_SIZE_IN_BYTES = 32
 
 
 @pytest.fixture(autouse=True)
@@ -26,48 +21,75 @@ def setup_and_teardown():
     system_test_common.stop_topology()
 
 
-def _client_peer_hub_status(client_name: str, hub_name: str):
-    topology_status = system_test_common.status_topology()
-    client_status = topology_status[("client", client_name)]
-    peer_hub_status = next(
-        (ph for ph in client_status["peer_hubs"] if ph["hub_name"] == hub_name),
-        None,
-    )
-    return peer_hub_status
-
-
-def _client_block_status(client_name: str, hub_name: str, local_or_peer: str):
-    assert local_or_peer in ("local", "peer")
-    # TODO: For now, assume there is only one local block
-    peer_hub_status = _client_peer_hub_status(client_name, hub_name)
-    print(f"{peer_hub_status=}", file=sys.stderr)
-
-    block_status = peer_hub_status[f"{local_or_peer}_pool"]["blocks"][0]
-    return block_status
-
-
 def _check_client_psrd_consumption(
     for_client_name: str,  # * for all clients
-    for_hub_name: str,  # * for all hubs
-    local_or_peer: str,
+    for_peer_hub_name: str,  # * for all hubs
+    for_local_or_peer: str,
     expected_consumed: int,
 ):
+    """
+    Check the PSRD consumption for a given pool (local or remote) on a given (or all) client(s)
+    for a given (or all) peer hub(s).
+    """
+    # pylint: disable=too-many-nested-blocks
+    topology_status = system_test_common.status_topology()
     for client_name in _CLIENTS:
-        for hub_name in _HUBS:
-            if client_name in ("*", for_client_name):
-                if hub_name in ("*", for_hub_name):
-                    block_status = _client_block_status(
-                        client_name, hub_name, local_or_peer
-                    )
-                    assert block_status["consumed"] == expected_consumed
+        if for_client_name in ("*", client_name):
+            client_status = topology_status[("client", client_name)]
+            for peer_hub_name in _HUBS:
+                if for_peer_hub_name in ("*", peer_hub_name):
+                    peer_hub_status = [
+                        ph
+                        for ph in client_status["peer_hubs"]
+                        if ph["hub_name"] == peer_hub_name
+                    ][0]
+                    for local_or_peer in ("local", "peer"):
+                        if for_local_or_peer in ("*", local_or_peer):
+                            # TODO: For now, assume one block per pool
+                            block_status = peer_hub_status[f"{local_or_peer}_pool"][
+                                "blocks"
+                            ][0]
+                            assert block_status["consumed"] == expected_consumed
+
+
+def _check_hub_psrd_consumption(
+    for_hub_name: str,  # * for all clients
+    for_peer_client_name: str,  # * for all hubs
+    for_local_or_peer: str,
+    expected_consumed: int,
+):
+    """
+    Check the PSRD consumption for a given pool (local or remote) on a given (or all) client(s)
+    for a given (or all) peer hub(s).
+    """
+    # pylint: disable=too-many-nested-blocks
+    topology_status = system_test_common.status_topology()
+    for hub_name in _HUBS:
+        if for_hub_name in ("*", hub_name):
+            hub_status = topology_status[("hub", hub_name)]
+            for peer_client_name in _CLIENTS:
+                if for_peer_client_name in ("*", peer_client_name):
+                    peer_client_status = [
+                        pc
+                        for pc in hub_status["peer_clients"]
+                        if pc["client_name"] == peer_client_name
+                    ][0]
+                    for local_or_peer in ("local", "peer"):
+                        if for_local_or_peer in ("*", local_or_peer):
+                            # TODO: For now, assume one block per pool
+                            block_status = peer_client_status[f"{local_or_peer}_pool"][
+                                "blocks"
+                            ][0]
+                            assert block_status["consumed"] == expected_consumed
 
 
 def test_psrd_usage_one_small_key():
     """
     Test PSRD usage when establishing one small key.
     """
-    # Check no PSRD consumed before getting key
-    _check_client_psrd_consumption("*", "*", "local", 0)
+    # Check no PSRD consumed at start (out-of-band messages are not signed nor encrypted)
+    _check_client_psrd_consumption("*", "*", "*", 0)
+    _check_hub_psrd_consumption("*", "*", "*", 0)
 
     # Get a small key (consume only PSRD from first block)
     key_size_in_bytes = 10
@@ -75,13 +97,19 @@ def test_psrd_usage_one_small_key():
     system_test_common.get_key_pair("carol", "cindy", size=key_size_in_bits)
 
     # Check PSRD consumption on master client Carol
-    local_consumed = _AUTHENTICATION_KEY_SIZE_IN_BYTES + key_size_in_bytes
-    _check_client_psrd_consumption("carol", "*", "local", local_consumed)
-    peer_consumed = _AUTHENTICATION_KEY_SIZE_IN_BYTES
-    _check_client_psrd_consumption("carol", "*", "peer", peer_consumed)
+    sign_size = _SIGNING_KEY_SIZE_IN_BYTES
+    sign_and_encrypt_size = _SIGNING_KEY_SIZE_IN_BYTES + key_size_in_bytes
+    _check_client_psrd_consumption("carol", "*", "local", sign_and_encrypt_size)
+    _check_client_psrd_consumption("carol", "*", "peer", sign_size)
 
     # Check PSRD consumption on slave client Cindy
-    local_consumed = _AUTHENTICATION_KEY_SIZE_IN_BYTES
-    _check_client_psrd_consumption("cindy", "*", "local", local_consumed)
-    peer_consumed = _AUTHENTICATION_KEY_SIZE_IN_BYTES + key_size_in_bytes
-    _check_client_psrd_consumption("cindy", "*", "peer", peer_consumed)
+    _check_client_psrd_consumption("cindy", "*", "local", sign_size)
+    _check_client_psrd_consumption("cindy", "*", "peer", sign_and_encrypt_size)
+
+    # Check PSRD consumption on each hub
+    for client in ["celia", "connie", "curtis"]:
+        _check_hub_psrd_consumption("*", client, "*", 0)
+    _check_hub_psrd_consumption("*", "carol", "local", sign_size)
+    _check_hub_psrd_consumption("*", "carol", "peer", sign_and_encrypt_size)
+    _check_hub_psrd_consumption("*", "cindy", "local", sign_and_encrypt_size)
+    _check_hub_psrd_consumption("*", "cindy", "peer", sign_size)
