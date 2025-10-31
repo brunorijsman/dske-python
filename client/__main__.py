50475b3e52aa7af8a77dff6f0fd961bc2830c07a
Bruno Rijsman <brunorijsman@hotmail.com>
2025-10-31 17:06:49 +0100

    Remove TODOs

--- client/__main__.py
+++ client/__main__.py
@@ -103,9 +103,7 @@ async def post_mgmt_stop():
     """
     Management: Post stop.
     """
-    # TODO: Can we delete the PID file later, when the process actually terminates?
     utils.delete_pid_file("client", _CLIENT.name)
-    # TODO: Process close listening socket when receive SIGTERM
     os.kill(os.getpid(), signal.SIGTERM)
     return {"result": "Client stopped"}
 
